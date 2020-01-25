9d433a8b39737c4cec9a1da763916a3d3b6e5a9c
MilanCFC <milcech@seznam.cz>
2020-01-25 14:36:42 +0100

    First attempt

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const readLineSync = require('readline-sync');
+const readLineSync = require("readline-sync");
 
-let numberOne = readLineSync.question('Write your number: ');
-
+let numberOne = readLineSync.question("Write your number: ");
 console.log(numberOne);
 
-console.log('my first change');
+let operator = readLineSync.question("Write your operator: ");
+console.log(operator);
 
-console.log('you are hacked');
+let numberTwo = readLineSync.question("Write your number: ");
+console.log(numberTwo);
+
+let result = oneBigResult {
+ if (operator === "+") {
+     result = numberOne + numberTwo;
+ } else if (operator === "-") {
+      result = numberOne - numberTwo;
+  } else if (operator === "/") {
+      result = numberOne / numberTwo;
+  } else if (operator === "*") {
+      result = numberOne * numberTwo;
+  }
+}
+console.log(result);
