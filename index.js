b2faaba43c0a6a40596e673a0696159baeaf4a02
MilanCFC <milcech@seznam.cz>
2020-01-25 18:23:51 +0100

    v1 should be done

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const readLineSync = require("readline-sync");
 
-let numberOne = readLineSync.question("Write your number: ");
+let numberOne = Number(readLineSync.question("Write your number: "));
 console.log(numberOne);
 
 let operator = readLineSync.question("Write your operator: ");
 console.log(operator);
 
-let numberTwo = readLineSync.question("Write your number: ");
+let numberTwo = Number(readLineSync.question("Write your number: "));
 console.log(numberTwo);
 
 function calculateResult() {
   let result = 0;
   if (operator === "+") {
-    result = Number(numberOne) + Number(numberTwo);
+    result = numberOne + numberTwo;
   } else if (operator === "-") {
-    result = Number(numberOne) - Number(numberTwo);
+    result = numberOne - numberTwo;
   } else if (operator === "/") {
-    result = Number(numberOne) / Number(numberTwo);
+    result = numberOne / numberTwo;
   } else if (operator === "*") {
-    result = Number(numberOne) * Number(numberTwo);
+    result = numberOne * numberTwo;
   }
   return result;
 }
